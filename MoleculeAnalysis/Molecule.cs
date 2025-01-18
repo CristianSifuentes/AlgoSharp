da7b532893844290bc89c18057129e6fd59e7aae
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2025-01-18 13:50:48 -0600

    Fix sln add reference for MoleculeAnalysis

--- MoleculeAnalysis/Molecule.cs
+++ MoleculeAnalysis/Molecule.cs
@@ -1,5 +1,6 @@
+using System.Collections.Generic;
+
 public class Molecule { 
-
-        public string Name { get; set; } // e.g., Water, Methane
-
+    public string Name { get; set; } // e.g., Water, Methane
+    public List<Atom> Atoms { get; set; } = new List<Atom>();
 }
