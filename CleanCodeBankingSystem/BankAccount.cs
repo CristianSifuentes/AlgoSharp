d4f5b2c675827c1b11cd6ed08a6642edae2f7a18
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2025-01-29 12:25:18 -0600

    DEfine 'contracts' Deposit, Withdraw and GetAccountDetails in IAccount interface after that implement 'dummy' methods in BankAccount

--- CleanCodeBankingSystem/BankAccount.cs
+++ CleanCodeBankingSystem/BankAccount.cs
@@ -1,4 +1,18 @@
 // Abstract base class for common account properties
-public abstract class BankAccount {
-    
+public abstract class BankAccount : IAccount
+{
+    public void Deposit(decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetAccountDetails()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        throw new NotImplementedException();
+    }
 }
--- CleanCodeBankingSystem/IAccount.cs
+++ CleanCodeBankingSystem/IAccount.cs
@@ -1,4 +1,6 @@
 // Interface for account operations
 public interface IAccount {
-    
+    void Deposit(decimal amount);
+    void Withdraw(decimal amount);
+    string GetAccountDetails();
 }
