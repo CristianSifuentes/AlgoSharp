57682c3044c9257a291930724f06099229a16998
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2025-01-30 19:47:18 -0600

    Add initial accounts with information dommy, creating constructor in BankAccount classes

--- CleanCodeBankingSystem/BankAccount.cs
+++ CleanCodeBankingSystem/BankAccount.cs
@@ -1,4 +1,14 @@
 // BankAccount entity (core business logic)
-public class BankAccount{  
-    
+public class BankAccount
+{
+    private string v1;
+    private string v2;
+    private int v3;
+
+    public BankAccount(string v1, string v2, int v3)
+    {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+    }
 }
--- CleanCodeBankingSystem/Program.cs
+++ CleanCodeBankingSystem/Program.cs
@@ -6,6 +6,11 @@
         var repository = new InMemoryBankAccountRepository();
         var depositUseCase = new DepositMoneyUseCase(repository);
         var reportUseCase = new GenerateReportUseCase(repository);
+        
+        // Add initial accounts
+        repository.Add(new BankAccount("1234", "Alice", 1000));
+        repository.Add(new BankAccount("67890", "Bob", 500));
+    
     }
 
 }
