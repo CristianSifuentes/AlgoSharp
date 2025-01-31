5020d2b7938e630171b6e2d66c513f4f081843b0
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2025-01-30 20:51:42 -0600

    Modify Execute method inside GenerateReportUseCase for select AccountHolder and Balance, modify InMemoryBankAccountRepository class modifying methods Add implemented logic for update dictionary, modify GetAll method for return Values, modify GetByAccountNumber method for return info, modify Update method for modify info inside dictionary, modify program for perfom deposit, generate report and simulate async work

--- CleanCodeBankingSystem/GenerateReportUseCase.cs
+++ CleanCodeBankingSystem/GenerateReportUseCase.cs
@@ -3,13 +3,12 @@ public class GenerateReportUseCase
 {
     private readonly IBankAccountRepository _repository;
 
-    public GenerateReportUseCase(IBankAccountRepository repository)
-    {
+    public GenerateReportUseCase(IBankAccountRepository repository){
         _repository = repository;
     }
 
     public IEnumerable<string> Execute(){
         var accounts = _repository.GetAll();
-         return (IEnumerable<string>)accounts;
+        return accounts.Select(a => $"{a.AccountHolder}: {a.Balance:C}");
     }
 }
--- CleanCodeBankingSystem/InMemoryBankAccountRepository.cs
+++ CleanCodeBankingSystem/InMemoryBankAccountRepository.cs
@@ -2,23 +2,28 @@
 
 public class InMemoryBankAccountRepository : IBankAccountRepository
 {
+    private readonly Dictionary<string, BankAccount> _accounts = new();
     public void Add(BankAccount bankAccount)
     {
-        throw new NotImplementedException();
+        if (_accounts.ContainsKey(bankAccount.AccountNumber))
+            throw new ArgumentException("Account already exists.");
+        _accounts[bankAccount.AccountNumber] = bankAccount;
     }
 
     public IEnumerable<BankAccount> GetAll()
     {
-        throw new NotImplementedException();
+        return _accounts.Values;
     }
 
     public BankAccount GetByAccountNumber(string accountNumber)
     {
-        throw new NotImplementedException();
+        _accounts.TryGetValue(accountNumber, out var account);
+        return account;
     }
 
-    public void Update(BankAccount bankAccount)
-    {
-        throw new NotImplementedException();
+    public void Update(BankAccount bankAccount){
+        if (!_accounts.ContainsKey(bankAccount.AccountNumber))
+            throw new ArgumentException("Account not found.");
+        _accounts[bankAccount.AccountNumber] = bankAccount;
     }
 }
