ea850ee3f484bc7e3bf3bd2e01e824e50b7ed0b8
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2025-02-02 23:47:01 -0600

    Methods 'Add' and 'Subtract' methods added, ValidateSameDimensions finished

--- CleanCodeMatrixOperations/MatrixOperations.cs
+++ CleanCodeMatrixOperations/MatrixOperations.cs
@@ -2,8 +2,16 @@
 public class MatrixOperations : IMatrixOperations
 {
     public Matrix Add(Matrix a, Matrix b)
-    {
-        throw new NotImplementedException();
+    { 
+        ValidateSameDimensions(a, b);
+        var result = new Matrix(a.Rows , a.Columns);
+        for (int i = 0; i < a.Rows; i++){
+            for (int j = 0; j < a.Columns; j++)
+            {
+                result[i, j] = a[i, j] + b[i, j];
+            }
+        }
+        return result;
     }
 
     public double Determinant(Matrix matrix)
@@ -18,11 +26,37 @@ public class MatrixOperations : IMatrixOperations
 
     public Matrix Subtract(Matrix a, Matrix b)
     {
-        throw new NotImplementedException();
+        ValidateSameDimensions(a, b);
+        var result = new Matrix(a.Rows, a.Columns);
+
+        for (int i = 0; i < a.Rows; i++){
+            for (int j = 0; j < a.Columns; j++){
+                result[i, j] = a[i, j] - b[i, j];
+            }
+        }
+
+        return result;
     }
 
     public Matrix Transpose(Matrix matrix)
     {
-        throw new NotImplementedException();
+        var result = new Matrix(matrix.Columns, matrix.Rows);
+
+        for (int i = 0; i < matrix.Rows; i++)
+        {
+            for (int j = 0; j < matrix.Columns; j++)
+            {
+                result[j, i] = matrix[i, j];
+            }
+        }
+        return result;
     }
+
+    private void ValidateSameDimensions(Matrix a, Matrix b)
+    {
+        if (a.Rows != b.Rows || a.Columns != b.Columns)
+            throw new InvalidOperationException("Matrices must have the same dimensions.");
+    }
+
+
 }
