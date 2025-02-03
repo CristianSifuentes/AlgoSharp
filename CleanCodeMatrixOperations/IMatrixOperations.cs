c9f82d632df71abf157a23d30cf12037e4f4707b
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2025-02-02 23:28:45 -0600

    Add IMatrixOperations 'contracts' and add 'dummy' implementation in MatrixOperations class

--- CleanCodeMatrixOperations/IMatrixOperations.cs
+++ CleanCodeMatrixOperations/IMatrixOperations.cs
@@ -1,5 +1,8 @@
 // Interface for matrix operations
 public interface IMatrixOperations{  
-
-
+    Matrix Add(Matrix a, Matrix b);
+    Matrix Subtract(Matrix a, Matrix b);
+    Matrix Multiply(Matrix a, Matrix b);
+    double Determinant(Matrix matrix);
+    Matrix Transpose(Matrix matrix);
 }
--- CleanCodeMatrixOperations/MatrixOperations.cs
+++ CleanCodeMatrixOperations/MatrixOperations.cs
@@ -1,5 +1,28 @@
 // Implementation of matrix operations
-public class MatrixOperations {
+public class MatrixOperations : IMatrixOperations
+{
+    public Matrix Add(Matrix a, Matrix b)
+    {
+        throw new NotImplementedException();
+    }
 
-    
+    public double Determinant(Matrix matrix)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Matrix Multiply(Matrix a, Matrix b)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Matrix Subtract(Matrix a, Matrix b)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Matrix Transpose(Matrix matrix)
+    {
+        throw new NotImplementedException();
+    }
 }
